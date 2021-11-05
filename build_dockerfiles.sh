cba6f53799bfed623d4c413927ab2d36a0504f50
Wolly <golliwogg@gmx.de>
2021-11-05 13:54:16 +0100

    improved dockerfiles

--- build_dockerfiles.sh
+++ build_dockerfiles.sh
@@ -2,8 +2,8 @@
 
 cd server
 docker build . -t myapp_server
-docker run --expose=9010 -p 9010:9010 -d myapp_server 9010
+docker run --expose=9010 -p 9010:9010 -d myapp_server ./run_server.sh 9010
 
 cd ../client
 docker build . -t myapp_client
-docker run --expose=9000 -p 9000:9000 -dit myapp_client 9000
+docker run --expose=9000 -p 9000:9000 -dit myapp_client ./run_client.sh 9000
