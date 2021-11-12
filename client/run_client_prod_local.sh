03cb6e86c73bcd10a30ec38a0a20666ec1711b62
Wolly <golliwogg@gmx.de>
2021-11-12 15:48:40 +0100

    fixed script + added script for client

--- client/run_client_prod_local.sh
+++ client/run_client_prod_local.sh
@@ -3,5 +3,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 docker build -t spread_client .
-docker build -t client_prod -f Dockerfile.Production
+docker build -t client_prod -f Dockerfile.Production .
 docker run -p 3000:3000 -it client_prod
